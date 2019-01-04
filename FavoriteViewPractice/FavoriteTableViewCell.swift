55d744040111acfb02a259aee410158ac02bb783
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2019-01-04 20:03:45 +0900

    日付テキストボックスまで実装

--- FavoriteViewPractice/FavoriteTableViewCell.swift
+++ FavoriteViewPractice/FavoriteTableViewCell.swift
@@ -7,15 +7,87 @@
 //
 
 import UIKit
+import FSPagerView
+import CHIPageControl
+import FaveButton
+
+import MaterialComponents.MaterialTextFields
 
 class FavoriteTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var reserveDateTextField: UITextField! {
+    
+    var allTextFieldControllers = [MDCTextInputControllerUnderline]()
+    
+    // TODO: デフォルト表示用ダミー画像を設定
+    var photos = ["1"]
+    
+    // VC内で呼ばれるCell拡張Delegate
+    weak var delegate: CollectionCellDelegate?
+    
+    @IBOutlet weak var favoriteButton: FaveButton!
+    
+    @IBOutlet weak var pagerView: FSPagerView! {
         didSet {
-            self.reserveDateTextField.placeholder = "日付を選択"
+            // 再利用する画像PagerViewCell設定
+            self.pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "PagerViewCell")
+            // 無限ループ設定
+            self.pagerView.isInfinite = true
+            // Automatic Slider
+            // pagerView.automaticSlidingInterval = 4
+            // エフェクト coverFlow .linear, .overlap は以下を有効
+            // pagerView.transformer = FSPagerViewTransformer(type: .linear)
+            // pagerView.itemSize = CGSize(width: 300, height: 200)
+            // pagerView.itemSize = FSPagerView.automaticSize
+            // pagerView.interitemSpacing = 16
+            // エフェクト .crossFading, .zoomOut, .depth のときは以下を有効
+            // pagerView.transformer = FSPagerViewTransformer(type: .zoomOut)
+            self.pagerView.itemSize = FSPagerView.automaticSize
+            self.pagerView.decelerationDistance = 1
         }
     }
-    /*
+    
+    @IBOutlet weak var pageControl: CHIPageControlAleppo! {
+        didSet {
+            // コントローラーUIViewサイズ
+            self.pageControl.frame = CGRect(x: 0, y:0, width: 100, height: 20)
+            // コントローラーUIViewの背景色
+            self.pageControl.backgroundColor = .none
+            // ドットの大きさ
+            self.pageControl.radius = 4
+            // ドットの幅
+            self.pageControl.padding = 8
+            // ドット色
+            self.pageControl.tintColor = .white
+            // アクティブなドット色
+            self.pageControl.currentPageTintColor = .white
+            // 非アクティブなドットの透明度
+            self.pageControl.inactiveTransparency = 0.8
+        }
+    }
+    
+    @IBOutlet weak var institutionLabelField: UILabel! {
+        didSet {
+            self.institutionLabelField.adjustsFontSizeToFitWidth = true
+            self.institutionLabelField.font = UIFont.boldSystemFont(ofSize: 24)
+        }
+    }
+    
+    @IBOutlet weak var localIdTextField: MDCTextField! {
+        didSet {
+            let fieldController = MDCTextInputControllerUnderline(textInput: self.localIdTextField)
+            fieldController.placeholderText = "診察券番号"
+            self.allTextFieldControllers.append(fieldController)
+        }
+    }
+    
+    @IBOutlet weak var reserveDateTextField: MDCTextField! {
+        didSet {
+            let fieldController = MDCTextInputControllerUnderline(textInput: self.reserveDateTextField)
+            fieldController.placeholderText = "次回予約日時メモ"
+            self.allTextFieldControllers.append(fieldController)
+        }
+    }
+    /* TODO: awakeFromNib中で設定しないとキーボードがセットできない。調査
     var reserveDatePicker: UIDatePicker! {
         didSet {
             
@@ -49,33 +121,89 @@ class FavoriteTableViewCell: UITableViewCell {
         // 分刻みを5分単位で設定
         self.reserveDatePicker.minuteInterval = 5
         
-        self.reserveDatePicker.addTarget(self, action:#selector(datePickerValueChanged), for: UIControl.Event.valueChanged)
-
+        self.reserveDatePicker.addTarget(self, action:#selector(hundleDatePickerValueChanged), for: UIControl.Event.valueChanged)
+        // UIDatePickerをキーボードに設定
         reserveDateTextField.inputView = reserveDatePicker
         
         // UIToolbarを設定
         let toolbar = UIToolbar()
         toolbar.sizeToFit()
         // Doneボタンを設定(押下時doneClickedが起動)
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(dateInputDone))
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(hundleDatePickerEditingDidEnd))
         // Doneボタンを追加
         toolbar.setItems([doneButton], animated: true)
-        
+        // UIToolbarをキーボードに設定
         reserveDateTextField.inputAccessoryView = toolbar
         
+        pagerView.dataSource = self
+        pagerView.delegate = self
+        favoriteButton.delegate = self
+        
+        pagerView.addSubview(pageControl)
+        pagerView.addSubview(favoriteButton)
+
     }
     
-    //datepickerが選択されたらtextfieldに表示
-    @objc func dateInputDone(sender:UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat  = "yyyy/MM/dd"
-        reserveDateTextField.text = dateFormatter.string(from: sender.date)
+    // Done
+    @objc func hundleDatePickerEditingDidEnd(sender:UIDatePicker) {
+        // let dateFormatter = DateFormatter()
+        // dateFormatter.dateFormat  = "yyyy/MM/dd HH:mm"
+        // reserveDateTextField.text = dateFormatter.string(from: sender.date)
     }
     
-    //datepickerが選択されたらtextfieldに表示
-    @objc func datePickerValueChanged(sender:UIDatePicker) {
+    // datepickerが変更されたらtextfieldに表示
+    @objc func hundleDatePickerValueChanged(sender:UIDatePicker) {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat  = "yyyy/MM/dd"
+        dateFormatter.dateFormat  = "yyyy/MM/dd HH:mm"
         reserveDateTextField.text = dateFormatter.string(from: sender.date)
+    }
+}
+
+// MARK: - CollectionCellDelegate
+protocol CollectionCellDelegate: class {
+    // VC内で呼ばれるFaveButtonDelegate拡張メソッド
+    func faveButton(_ cell: FavoriteTableViewCell, faveButton: FaveButton, didSelected selected: Bool)
+}
+
+// MARK: - FaveButtonDelegate
+extension FavoriteTableViewCell: FaveButtonDelegate {
+    // VC内でCell情報を利用できるようFaveButtonDelegateを拡張
+    func faveButton(_ faveButton: FaveButton, didSelected selected: Bool) {
+        delegate?.faveButton(self, faveButton:faveButton, didSelected: selected)
+    }
+}
+
+// 画像カルーセル処理。UI部分のみなのでCell内で実装
+// MARK: - FSPagerViewDataSource, FSPagerViewDelegate
+extension FavoriteTableViewCell: FSPagerViewDataSource, FSPagerViewDelegate {
+    
+    func numberOfItems(in pagerView: FSPagerView) -> Int {
+        // pageControlドット数
+        pageControl.numberOfPages = photos.count
+        return photos.count
+    }
+    
+    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "PagerViewCell", at: index)
+        
+        // cell.contentView.layer.shadowOpacity = 0.4
+        // cell.contentView.layer.opacity = 0.86
+        cell.imageView?.image = UIImage(named: self.photos[index])
+        cell.imageView?.contentMode = .scaleAspectFill
+        cell.imageView?.clipsToBounds = true
+        return cell
+    }
+    
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        pagerView.deselectItem(at: index, animated: true)
+        pagerView.scrollToItem(at: index, animated: true)
+    }
+    
+    func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
+        pageControl.set(progress: targetIndex, animated: true)
+    }
+    
+    func pagerViewDidEndScrollAnimation(_ pagerView: FSPagerView) {
+        pageControl.set(progress: pagerView.currentIndex, animated: true)
     }
 }
--- FavoriteViewPractice/FavoriteViewController.swift
+++ FavoriteViewPractice/FavoriteViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import FaveButton
+import MaterialComponents.MaterialSnackbar
+import MaterialComponents.MaterialSnackbar_ColorThemer
+import MaterialComponents.MaterialSnackbar_TypographyThemer
+
 
 class FavoriteViewController: UIViewController {
 
     @IBOutlet weak var favoriteTableView: UITableView!
     
-    let photos = ["1","2","3","4","5","6","7"]
+    let photos = ["3","4","5","6","7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +28,48 @@ class FavoriteViewController: UIViewController {
     }
 
     private func setupTableView() {
-        favoriteTableView?.register(UINib(nibName: "FavoriteTableViewCell", bundle: nil), forCellReuseIdentifier: "tableCell")
+        favoriteTableView.register(UINib(nibName: "FavoriteTableViewCell", bundle: nil), forCellReuseIdentifier: "tableCell")
+        // Cell選択時の背景色変更を解除
+        favoriteTableView.allowsSelection = false
         // Cell区切り線を消す
         favoriteTableView.separatorStyle = .none
-        favoriteTableView?.dataSource = self
-        favoriteTableView?.delegate = self
+        favoriteTableView.dataSource = self
+        favoriteTableView.delegate = self
     }
 }
 
 extension FavoriteViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // return photos.count
-        return 1
+        return photos.count
+        //return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = favoriteTableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! FavoriteTableViewCell
+        cell.delegate = self
+        // 画像を設定
+        cell.photos = self.photos
+        // TODO: セルの背景色をランダムに設定する。（後で削除）
+        // cell.backgroundColor = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
 
         return cell
     }
 }
+
+// MARK: - CollectionCellDelegate
+extension FavoriteViewController: CollectionCellDelegate {
+    
+    func faveButton(_ cell: FavoriteTableViewCell, faveButton: FaveButton, didSelected selected: Bool) {
+        // TODO: お気に入りボタンタップ処理
+        if selected {
+            // 画面下部トースト表示
+            let toastMessage = MDCSnackbarMessage()
+            toastMessage.text = "かかりつけの病院に登録しました"
+            MDCSnackbarManager.show(toastMessage)
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+}
+
